e42625bd4f3853b2d80f71491f2181f2872f01a4
Codewithdeepak07 <121105420+Codewithdeepak07@users.noreply.github.com>
2024-08-31 06:35:15 +0000

    dataTypes

---
+++ 01_basis/01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; ///treat all JS code as newer version
+
+
+// alert(3 + 3) // we are using nodejs, not browser
+console.log(3+4)
+
+let name = "Deepak"
+let age = 20
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string => " " ,' '
+//boolean =>true,false
+//null => standalone value(empty)
+//undefined => no value assign
+//symbol => unique
+
+
+//object 
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
