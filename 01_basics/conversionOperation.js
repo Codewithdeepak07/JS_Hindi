596cfc3dd9dd38dfe85371820e391684b4e0dd34
Codewithdeepak07 <121105420+Codewithdeepak07@users.noreply.github.com>
2024-08-31 14:19:38 +0000

    Conversion Operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,27 +1,27 @@
-// let score = undefined
-// console.log(typeof score)
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+let score = undefined
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
 
 // "33" => 33
 // "3abc" => NaN(not a number)
 // true = 1, false = 0
 //null => 0
 
-// let isLoggedIn = "f"
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(typeof booleanIsLoggedIn)
-// console.log(booleanIsLoggedIn)
+let isLoggedIn = "f"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
 
 //1 => true, 0 => false
 // "" => false
 //"Deepak" =>true
 
-// let number = 33
-// let stringNumber = String(number)
-// console.log(typeof stringNumber)
-// console.log(stringNumber)
+let number = 33
+let stringNumber = String(number)
+console.log(typeof stringNumber)
+console.log(stringNumber)
 
 
 // ********************* Operations ***************
@@ -34,7 +34,10 @@ let s1 = "Hello"
 let s2 = " Deepak"
 console.log(s1+s2)
 
-console.log("1" + 2)
-console.log(1 + "2")
-console.log("1" + 2 + 2)
-console.log(1 + 2 + "2")
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
