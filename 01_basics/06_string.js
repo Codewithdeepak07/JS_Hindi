f89ec4ecd78bc0f5053225642e6daab61b2040eb
Codewithdeepak07 <121105420+Codewithdeepak07@users.noreply.github.com>
2024-09-01 15:39:07 +0000

    String Function

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -2,7 +2,7 @@ const name = "Deepak"
 const repoCount = 3
 // console.log(name  + repoCount + "Value")
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)//string interpulation
-const game = new String('Marvel')
+const game = new String('MarvelHero')
 console.log(game[0])
 // console.log(game.__proto__)
 // console.log(game.toUpperCase())
@@ -11,4 +11,22 @@ console.log(game[0])
 
 const newString = game.substring(0,4)
 console.log(newString)
+
+const anotherString = game.slice(-8,3)
+console.log(anotherString)
+
+const newStringOne = "   Deep   "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://google%20.com"
+console.log(url.replace('%20','--' ))
+console.log(url.includes('google'))
+
+const str = 'The quick brown fox jump'
+console.log(str.split(' '))
+
+
+
+
 
