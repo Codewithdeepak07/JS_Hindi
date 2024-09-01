4457aa7910f1c873b20761178de8fde9f0e9e53f
Codewithdeepak07 <121105420+Codewithdeepak07@users.noreply.github.com>
2024-09-01 08:52:04 +0000

    String

---
+++ 01_basics/06_string.js
@@ -0,0 +1,14 @@
+const name = "Deepak"
+const repoCount = 3
+// console.log(name  + repoCount + "Value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)//string interpulation
+const game = new String('Marvel')
+console.log(game[0])
+// console.log(game.__proto__)
+// console.log(game.toUpperCase())
+// console.log(game.charAt(3))
+// console.log(game.indexOf('r'))
+
+const newString = game.substring(0,4)
+console.log(newString)
+
